c7bb13d555ca0143466acd0cf74e7afd55556a4e
Benjamin Brooks <benjaminbrooks@Benjamins-MacBook-Pro.local>
2014-10-14 12:03:09 -0700

    Removing unnecessary figure.js file

--- js/figure.js
+++
@@ -1,9 +0,0 @@
-var sankey = d3.sankey()
-    .size([width, height])
-    .nodeWidth(15)
-    .nodePadding(10)
-    .nodes(energy.nodes)
-    .links(energy.links)
-    .layout(32);
-var path = sankey.link();
-
